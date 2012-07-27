f7052407dba676c554a6821d1a5979d44b65a521
Justin Mazzi <jmazzi@gmail.com>
2012-07-26 21:35:24 -0400

    Add docs

--- lib/crypt_keeper_providers/aes.rb
+++ lib/crypt_keeper_providers/aes.rb
@@ -6,6 +6,9 @@ module CryptKeeperProviders
   class Aes
     attr_accessor :key, :aes
 
+    # Public: Initializes the class
+    #
+    #   options - A hash of options. :passphrase is required
     def initialize(options = {})
       @key         = options.delete :passphrase
       @aes         = ::OpenSSL::Cipher::Cipher.new("AES-256-CBC")
@@ -13,6 +16,9 @@ module CryptKeeperProviders
       @key         = Digest::SHA1.hexdigest(key).unpack('a2'*32).map{|x|x.hex}.pack('c'*32)
     end
 
+    # Public: Encrypt a string
+    #
+    # Returns a string
     def encrypt(value)
       value = value.to_s
       aes.encrypt
@@ -20,12 +26,14 @@ module CryptKeeperProviders
       Base64::encode64(aes.update(value) + aes.final)
     end
 
+    # Public: Decrypt a string
+    #
+    # Returns a string
     def decrypt(value)
       value = Base64::decode64(value.to_s)
       aes.decrypt
       aes.key = key
       aes.update(value) + aes.final
     end
-
   end
 end
