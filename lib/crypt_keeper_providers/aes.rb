eddd9f20ba09a6b9da0957b3873536c8a0b7675f
Justin Mazzi <jmazzi@gmail.com>
2012-07-28 12:52:55 -0400

    Code cleanup

--- lib/crypt_keeper_providers/aes.rb
+++ lib/crypt_keeper_providers/aes.rb
@@ -6,7 +6,11 @@ module CryptKeeperProviders
   class Aes
     SEPARATOR = ":crypt_keeper:"
 
-    attr_accessor :key, :aes
+    # Public: The encryption key
+    attr_accessor :key
+
+    # Public: An instance of  OpenSSL::Cipher::Cipher
+    attr_accessor :aes
 
     # Public: Initializes the class
     #
@@ -22,23 +26,21 @@ module CryptKeeperProviders
     #
     # Returns a string
     def encrypt(value)
-      value = value.to_s
       aes.encrypt
       aes.key = key
-      iv = rand.to_s
-      aes.iv = iv
-      Base64::encode64("#{iv}#{SEPARATOR}#{aes.update(value) + aes.final}")
+      iv      = rand.to_s
+      aes.iv  = iv
+      Base64::encode64("#{iv}#{SEPARATOR}#{aes.update(value.to_s) + aes.final}")
     end
 
     # Public: Decrypt a string
     #
     # Returns a string
     def decrypt(value)
-      value = Base64::decode64(value.to_s)
-      iv, value = value.split(SEPARATOR)
+      iv, value = Base64::decode64(value.to_s).split(SEPARATOR)
       aes.decrypt
       aes.key = key
-      aes.iv = iv
+      aes.iv  = iv
       aes.update(value) + aes.final
     end
   end
