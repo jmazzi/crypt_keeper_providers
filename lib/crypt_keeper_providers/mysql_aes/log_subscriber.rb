f8b6f8739329e125542dd7fe69407c98e1a9f769
Justin Mazzi <jmazzi@gmail.com>
2012-07-30 20:23:39 -0400

    Adding mysql_aes

--- lib/crypt_keeper_providers/mysql_aes/log_subscriber.rb
+++ lib/crypt_keeper_providers/mysql_aes/log_subscriber.rb
@@ -2,26 +2,26 @@ require 'active_support/concern'
 require 'active_support/lazy_load_hooks'
 
 module CryptKeeperProviders
-  module PostgresPgpLogSubscriber
+  module MysqlAesLogSubscriber
     extend ActiveSupport::Concern
 
     included do
-      alias_method_chain :sql, :postgres_pgp
+      alias_method_chain :sql, :mysql_aes
     end
 
     # Public: Prevents sensitive data from being logged
-    def sql_with_postgres_pgp(event)
-      filter = /(pgp_sym_(encrypt|decrypt))\(((.|\n)*?)\)/i
+    def sql_with_mysql_aes(event)
+      filter = /(aes_(encrypt|decrypt))\(((.|\n)*?)\)/i
 
       event.payload[:sql] = event.payload[:sql].gsub(filter) do |_|
         "#{$1}([FILTERED])"
       end
 
-      sql_without_postgres_pgp(event)
+      sql_without_mysql_aes(event)
     end
   end
 end
 
 ActiveSupport.on_load :active_record do
-  ActiveRecord::LogSubscriber.send :include, CryptKeeperProviders::PostgresPgpLogSubscriber
+  ActiveRecord::LogSubscriber.send :include, CryptKeeperProviders::MysqlAesLogSubscriber
 end
